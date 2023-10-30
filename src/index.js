83ba854a72018331bcde2ecf74a7de74c79d6d0c
beatricenjerimwangi <bbeatricemwangi@gmail.com>
2023-10-30 13:56:23 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    taskAdder(e.target.new_task_description.value)
+  })
 });
+function taskAdder(task){
+  let listitem= document.createElement(`li`)
+  let p = document.createElement(`p`)
+  p.textContent=` ${task}`
+  let btn = document.createElement(`button`)
+  btn.textContent=`X`
+  btn.addEventListener(`click`,deleteTask)
+  p.appendChild(btn)
+  listitem.appendChild(p)
+  document.querySelector(`#tasks`).appendChild(listitem)
+}
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
